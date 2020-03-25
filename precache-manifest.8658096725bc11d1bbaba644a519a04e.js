0c346b7817185f6bfdda4272303145c8a5982b69
sandhya05 <sandhyamohan90@gmail.com>
2020-03-25 14:58:51 +0530

    Updates

---
+++ precache-manifest.8658096725bc11d1bbaba644a519a04e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd9792a241c801e3414d8b32b2c8b27c",
+    "url": "/utube_clone/index.html"
+  },
+  {
+    "revision": "4210593039324cee9e1f",
+    "url": "/utube_clone/static/js/2.d718a644.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/utube_clone/static/js/2.d718a644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b1ce37cc66dd504ba50",
+    "url": "/utube_clone/static/js/main.883918f9.chunk.js"
+  },
+  {
+    "revision": "64143ed6e0d346afa44e",
+    "url": "/utube_clone/static/js/runtime-main.b8551c8c.js"
+  }
+]);
